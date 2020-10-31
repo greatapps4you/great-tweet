51269d296450f9f87d03d280031962559e749af0
Jose E. de Souza N <developer@greatapps4you.us>
2020-10-31 16:43:49 +0100

    [great-tweet-entities] user and message

---
+++ great-tweet-entities/src/main/java/us/greatapps4you/greattweet/entities/Message.java
@@ -0,0 +1,66 @@
+package us.greatapps4you.greattweet.entities;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    private int id;
+    private String content;
+    private LocalDateTime publicationTime;
+
+    public Message(String content, LocalDateTime publicationTime) {
+        this.content = content;
+        this.publicationTime = publicationTime;
+    }
+
+    public Message(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getPublicationTime() {
+        return publicationTime;
+    }
+
+    public void setPublicationTime(LocalDateTime publicationTime) {
+        this.publicationTime = publicationTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Message message = (Message) o;
+
+        if (!content.equals(message.content)) return false;
+        return publicationTime.equals(message.publicationTime);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = content.hashCode();
+        result = 31 * result + publicationTime.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "content='" + content + '\'' +
+                ", publicationTime=" + publicationTime +
+                '}';
+    }
+}
---
+++ great-tweet-entities/src/main/java/us/greatapps4you/greattweet/entities/User.java
@@ -0,0 +1,86 @@
+package us.greatapps4you.greattweet.entities;
+
+import java.util.List;
+
+public class User {
+    private int id;
+    private String userName;
+    private String nickName;
+    private List<User> following;
+    private List<Message> messages;
+
+    public User(String userName, String nickName, List<User> following, List<Message> messages) {
+        this.userName = userName;
+        this.nickName = nickName;
+        this.following = following;
+        this.messages = messages;
+    }
+
+    public User(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public List<User> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(List<User> following) {
+        this.following = following;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!userName.equals(user.userName)) return false;
+        return nickName.equals(user.nickName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userName.hashCode();
+        result = 31 * result + nickName.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", nickName='" + nickName + '\'' +
+                '}';
+    }
+}
