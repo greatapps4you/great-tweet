43420fc7030cb1607b3547c56473c6241e43c2cb
Jose E. de Souza N <developer@greatapps4you.us>
2020-11-02 14:40:31 +0100

    [great-tweet-application] PostingControllerTest service status

--- great-tweet-application/src/main/java/us/greatapps4you/greattweet/application/rest/PostingController.java
+++ great-tweet-application/src/main/java/us/greatapps4you/greattweet/application/rest/PostingController.java
@@ -1,4 +1,26 @@
 package us.greatapps4you.greattweet.application.rest;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import us.greatapps4you.greattweet.entities.Message;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/api")
 public class PostingController {
+
+    @GetMapping("/posting")
+    @ResponseBody
+    public EntityModel serviceStatus() {
+        return EntityModel.of(new Message("OK", LocalDateTime.now()),
+                linkTo(methodOn(PostingController.class).serviceStatus()).withSelfRel());
+    }
+
 }
---
+++ great-tweet-application/src/test/java/us/greatapps4you/greattweet/application/rest/PostingControllerTest.java
@@ -0,0 +1,32 @@
+package us.greatapps4you.greattweet.application.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PostingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/api/posting"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("content", is("OK")));
+    }
+
+}
