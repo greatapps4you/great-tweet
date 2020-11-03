d813257916eeda8dcb18f1ac1c54b7572917c8c7
Jose E. de Souza N <developer@greatapps4you.us>
2020-11-03 23:42:08 +0100

    [great-tweet-application] Following Feature added to application

--- great-tweet-application/src/main/java/us/greatapps4you/greattweet/application/rest/FollowingController.java
+++ great-tweet-application/src/main/java/us/greatapps4you/greattweet/application/rest/FollowingController.java
@@ -1,4 +1,36 @@
 package us.greatapps4you.greattweet.application.rest;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import us.greatapps4you.greattweet.application.persistence.FollowingServiceWithJPA;
+import us.greatapps4you.greattweet.entities.User;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/api")
 public class FollowingController {
+
+    private final FollowingServiceWithJPA followingService;
+
+    public FollowingController(FollowingServiceWithJPA followingService) {
+        this.followingService = followingService;
+    }
+
+    @PostMapping(path = "/following/{followed}",
+            consumes = "application/json",
+            produces = "application/hal+json")
+    public ResponseEntity<EntityModel<User>> followUser(@RequestBody User me, @PathVariable String followed) {
+        User followedUser = followingService.followUser(me, new User(followed, followed));
+        if (followedUser == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok().body(EntityModel.of(followedUser,
+                    linkTo(methodOn(UsersController.class)
+                            .findByUniqueName(followedUser.getUniqueName())).withSelfRel()));
+        }
+    }
+
 }
---
+++ great-tweet-application/src/test/java/us/greatapps4you/greattweet/application/rest/FollowingControllerTest.java
@@ -0,0 +1,81 @@
+package us.greatapps4you.greattweet.application.rest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import us.greatapps4you.greattweet.entities.User;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class FollowingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    private User followed;
+    private User me;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        followed = new User("emma", "Emma Watson");
+        TweetPostingTO emmaFirstTweet = new TweetPostingTO("emma", "I am Emma Watson, but you can call me beauty");
+        String emmaFirstTweetJson = objectToJson(emmaFirstTweet);
+        // Currently the only way to creat an user is when he posts a tweet
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/posting")
+                .content(emmaFirstTweetJson)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isCreated());
+
+        me = new User("josethedeveloper", "Jose Esteves");
+        TweetPostingTO joseFirstTweet =
+                new TweetPostingTO("josethedeveloper",
+                        "I have developed this system and now I follow Emma Watson");
+        String joseFirstTweetJson = objectToJson(joseFirstTweet);
+        // Currently the only way to creat an user is when he posts a tweet
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/posting")
+                .content(joseFirstTweetJson)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void whenFollowUser_ThenReturnFollowed() throws Exception {
+        // I go on the payload and the user I followed goes on the path
+        String meJson = objectToJson(me);
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/following/" + followed.getUniqueName())
+                .content(meJson)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON_VALUE))
+                .andDo(print())
+        .andExpect(status().isOk());
+    }
+
+    private String objectToJson(Object object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter objectWriter = mapper.writer().withDefaultPrettyPrinter();
+        return objectWriter.writeValueAsString(object);
+    }
+
+}
